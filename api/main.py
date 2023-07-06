3dfdc9809ffa9ccae2d38391a32f914ed32478ce
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-07 01:48:20 +0330

    Now ready to work on executing scripts

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import os
+
 
 from api.config.config import path
 from api.database.sqlite import database
@@ -12,103 +14,113 @@ CORS(app)
 
 @app.route('/api/users', methods=['GET'])
 def all_users():
+    response = {}
+
     cursor, connection = database()
 
-    cursor.execute("SELECT * FROM User")
-    users = cursor.fetchall()
+    cursor.execute("SELECT * FROM USERS")
+    execution = cursor.fetchall()
+
+    cursor.execute(
+        "SELECT COUNT(*) AS cAll, \
+        SUM(CASE WHEN isAdmin = 1 THEN 1 ELSE 0 END) AS cAdmin, \
+        SUM(CASE WHEN isAdmin = 0 THEN 1 ELSE 0 END) AS cUsers \
+        FROM USERS"
+    )
+
+    result = cursor.fetchone()
+
+    cAll = result[0]
+    cAdmins = result[1]
+    cUsers = result[2]
+
+    count = {
+        "total": cAll,
+        "users": cUsers,
+        "admins": cAdmins
+    }
+
+    response['count'] = count
+
+    users = []
+
+    for record in execution:
+        user = {
+            "id": record[0],
+            "name": record[1],
+            "password": record[2],
+            "isAdmin": record[3]
+        }
+        users.append(user)
 
     connection.close()
 
-    return jsonify(users), 200
+    response['data'] = users
 
-    # script_path = os.path.join(path, 'scripts\\test.sh')
-    # execution = execute(script_path, "amir")
-
-    # status_code = 0
-
-    # if execution:
-    #     status_code = 200
-    #     response['status'] = 'success'
-    # else:
-    #     status_code = 500
-    #     response['status'] = 'error'
-
-    # return jsonify(response), status_code
+    return jsonify(response), 200
 
 
-@app.route('/api/users', methods=['POST'])
+@ app.route('/api/users', methods=['POST'])
 def create_user():
+    response = {}
+
     cursor, connection = database()
 
+    username = request.json['username']
+    password = request.json['password']
+    isAdmin = request.json['isAdmin']
+
+    script_path = os.path.join(path, 'scripts/test.sh')
+    execution = execute(script_path, "amir")
+
+    if execution:
+        response['status'] = 'success'
+    else:
+        response['status'] = 'error'
+
+    cursor.execute(
+        'INSERT INTO USERS (username, password, isAdmin) VALUES (?, ?, ?)', (username, password, isAdmin))
+
+    connection.commit()
+    connection.close()
+
+    response['message'] = "User created"
+
+    return jsonify(response), 200
+
+
+@ app.route('/api/users/<id>', methods=['PATCH'])
+def update_user(id):
     response = {}
-    status_code = 0
+
+    cursor, connection = database()
 
     username = request.json['username']
     password = request.json['password']
 
     cursor.execute(
-        'INSERT INTO User (username, password) VALUES (?, ?)', (username, password))
+        'UPDATE USERS SET username = ?, password = ? WHERE id = ?', (username, password, id))
+
     connection.commit()
     connection.close()
 
-    response['message'] = "User created"
-    status_code = 200
+    response['message'] = "User updated"
 
-    return jsonify(response), status_code
-
-
-@app.route('/api/users/<id>', methods=['PATCH'])
-def update_user(id):
-    cursor, connection = database()
-
-    response = {}
-
-    try:
-        cursor.execute("SELECT * FROM User WHERE id = ?", (id))
-
-        if (len(cursor.fetchall()) > 0):
-            print("Yes")
-        else:
-            raise Exception({'message': 'User not found', 'status_code': 404})
-
-        try:
-            username = request.json['username']
-            password = request.json['password']
-
-            cursor.execute(
-                'UPDATE User SET username = ?, password = ? WHERE id = ?', (username, password, id))
-            connection.commit()
-
-            response['message'] = "User updated"
-
-            return jsonify(response), 200
-        except Exception as e:
-            response['message'] = e
-
-            return jsonify(response), 500
-        finally:
-            connection.close()
-    except Exception as e:
-        response['message'] = str(e)
-
-        return jsonify(response), 404
-    finally:
-        connection.close()
+    return jsonify(response), 200
 
 
-@app.route('/api/users/<id>', methods=['DELETE'])
+@ app.route('/api/users/<id>', methods=['DELETE'])
 def delete_user(id):
-    cursor, connection = database()
-
     response = {}
-    status_code = 0
+
+    cursor, connection = database()
 
     cursor.execute(
-        'DELETE FROM User WHERE id = ?', (id))
+        'DELETE FROM USERS WHERE id = ?', (id))
+
     connection.commit()
     connection.close()
 
     response['message'] = "User deleted"
-    status_code = 200
 
-    return jsonify(response), status_code
+    return jsonify(response), 200
