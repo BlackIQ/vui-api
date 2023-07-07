f7a92cc3689bd7e4762bc7083fa2eb417dbc0478
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-07 05:49:30 +0330

    Hope it works

--- api/functions/message.py
+++ api/functions/message.py
@@ -1,18 +1,30 @@
-import telegram
-import asyncio
+# import telegram
+
+# import asyncio
+
+import requests as req
 
 bot_token = '6058827294:AAEUcx1fc8quG4py_AVOMLbYYXDLiFmID_s'
 
-bot = telegram.Bot(token=bot_token)
+# bot = telegram.Bot(token=bot_token)
 
 
-async def final_send(message, chatid):
-    await bot.send_message(chat_id=chatid, text=message)
+# async def final_send(message, chatid):
+#     await bot.send_message(chat_id=chatid, text=message)
 
 
-async def main(message, chatid):
-    await final_send(message, chatid)
+# async def main(message, chatid):
+#     await final_send(message, chatid)
 
+
+# def send(message, chatid):
+#     asyncio.run(main(message, chatid))
 
 def send(message, chatid):
-    asyncio.run(main(message, chatid))
+    data = {
+        "chat_id": chatid,
+        "text": message
+    }
+
+    req.post(
+        f"https://api.telegram.org/bot/{bot_token}/sendMessage", data=data)
