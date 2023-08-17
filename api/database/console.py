cad94f2ae9fb72398b2fb8ccc967f3676999d8ea
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-08-17 18:27:53 +0330

    Add console

--- api/database/console.py
+++ api/database/console.py
@@ -1,15 +1,11 @@
-import datetime
 import sqlite3
 
 # Connect to the SQLite database
 conn = sqlite3.connect('vui.db')
 cursor = conn.cursor()
 
-timestamp = datetime.datetime.now()
-expire = timestamp + datetime.timedelta(days=30)
-
 # Update the users table
-cursor.execute("UPDATE USERS SET expire= ?", (expire,))
+cursor.execute("UPDATE USERS SET access = ?", (True,))
 
 # Commit the changes and close the connection
 conn.commit()
