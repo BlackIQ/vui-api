aeb97c6c0e7c686f2ea9f5d105962c4befac6bd8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-05-04 00:48:14 +0330

    Really?

--- api/database/create.py
+++ api/database/create.py
@@ -4,18 +4,31 @@ import sqlite3
 conn = sqlite3.connect('vui.db')
 cursor = conn.cursor()
 
+# Create the log table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS LOGS (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(50),
+        action VARCHAR(50),
+        role VARCHAR(50),
+        level INT,
+        creator VARCHAR(50)
+    )
+''')
+
 # Create the users table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS USERS (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username VARCHAR(50),
         password VARCHAR(50),
+        name VARCHAR(50),
         role VARCHAR(50),
         chatid INT,
         owner VARCHAR(50),
         timestamp DATETIME,
         expire DATETIME,
-        access BOOLEAN,
+        access BOOLEAN
     )
 ''')
 
