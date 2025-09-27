bdaf334047b1466835343b12c295fd2255b7d261
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-09-27 13:08:20 +0330

    Update: Configs: WOW

--- api/config/config.py
+++ api/config/config.py
@@ -1,10 +1,4 @@
-from dotenv import load_dotenv
 import sys
-import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Access environment variables
-env = os.environ
 path = sys.path[0]
+DB_NAME = "vui.db"
