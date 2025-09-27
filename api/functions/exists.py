8a65aefe110be76dfa87be2bee8d754f82416379
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-09-27 13:07:43 +0330

    Delete: Functions: Remove useless stuff

--- api/functions/exists.py
+++ api/functions/exists.py
@@ -3,7 +3,7 @@ from api.database.sqlite import database
 def exists(username):
     cursor, connection = database()
 
-    cursor.execute(f"SELECT COUNT(*) AS c FROM USERS WHERE username = ?", (username,))
+    cursor.execute(f"SELECT COUNT(*) AS c FROM CLIENTS WHERE username = ?", (username,))
 
     count = cursor.fetchone()[0]
 
